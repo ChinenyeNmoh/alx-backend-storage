472c025ae7137dd7fcb65b547c37500c1af1a868
Chinenye Nmoh <chinenyeumeaku@gmail.com>
2023-12-18 19:38:20 +0100

    NoSQL

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python3
+"""Write a Python function that inserts a new document
+in a collection based on kwargs:
+
+Prototype: def insert_school(mongo_collection, **kwargs):
+mongo_collection will be the pymongo collection object
+Returns the new _id
+"""
+
+
 def insert_school(mongo_collection, **kwargs):
     """Insert a school document into the MongoDB collection."""
     result = mongo_collection.insert_one(kwargs)
